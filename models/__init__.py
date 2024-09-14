29d8b01e1cd58865adfe3c4d7ab4e88bad003f23
Flo-renc <f.kabeya@alustudent.com>
2024-09-14 23:02:36 +0300

    debugging console.py

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,6 @@
 This module initializes the models package and creates an instance of FileStorage.
 """
 from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
